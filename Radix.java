8e689b3760f6993f70869f208fee64c516f6de58
lauk20 <klau20@stuy.edu>
2021-01-12 11:59:55 -0500

    Added length() method

--- Radix.java
+++ Radix.java
@@ -2,4 +2,11 @@ public class Radix{
   public static int nth(int n, int col){
     return (int)((Math.abs(n) / Math.pow(10, col)) % 10);
   }
+
+  public static int length(int n){
+    if (n == 0){
+      return 1;
+    }
+    return (int)(Math.log10(Math.abs(n))) + 1;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -4,5 +4,14 @@ public class Tester{
     System.out.println(Radix.nth(-123,1));
     System.out.println(Radix.nth(123,2));
     System.out.println(Radix.nth(-123,2));
+
+    System.out.println();
+
+    System.out.println(Radix.length(0));
+    System.out.println(Radix.length(9));
+    System.out.println(Radix.length(10));
+    System.out.println(Radix.length(15));
+    System.out.println(Radix.length(-10));
+    System.out.println(Radix.length(5112));
   }
 }
