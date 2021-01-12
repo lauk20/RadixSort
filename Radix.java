6d3d8dc64e6ea428cf6c1d9759a99128ea580515
lauk20 <klau20@stuy.edu>
2021-01-12 16:23:28 -0500

    radixSortSimple() method added and tests. Seems to be working

--- Radix.java
+++ Radix.java
@@ -10,9 +10,46 @@ public class Radix{
     return (int)(Math.log10(Math.abs(n))) + 1;
   }
 
-  public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
     for (int i = 0; i < buckets.length; i++){
       original.extend(buckets[i]);
     }
   }
+
+  public static void radixSortSimple(SortableLinkedList data){
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+
+    for (int i = 0; i < 10; i++){
+      buckets[i] = new SortableLinkedList();
+    }
+
+    int largestDigit = 0;
+
+    for (int i = 0; i < data.size(); i++){
+      int item = data.get(i);
+      int digits = length(item);
+
+      if (digits > largestDigit){
+        largestDigit = digits;
+      }
+
+      buckets[nth(item, 0)].add(item);
+      data.remove(i);
+      i = i - 1;
+    }
+
+    merge(data, buckets);
+
+    for (int i = 1; i < largestDigit; i++){
+      for (int j = 0; j < data.size(); j++){
+        int item = data.get(0);
+
+        buckets[nth(item, i)].add(item);
+        data.remove(0);
+        j = j - 1;
+      }
+      merge(data, buckets);
+    }
+  }
+
 }
